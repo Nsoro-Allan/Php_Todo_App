f31374740a96e392ee9b3097cb1b3b6136bd62b0
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-04-02 12:13:26 +0200

    done...

--- home.php
+++ home.php
@@ -14,7 +14,64 @@ include("connection.php");
     <link rel="shortcut icon" href="./images/icon.ico" type="image/x-icon">
 </head>
 <body>
-    <h1>Welcome, <?php echo $_SESSION['todo_user'];?></h1>
-    <a href="./logout">Logout</a>
+    <div class="main-container">
+
+        <div class="container">
+
+            <div class="head">
+
+                <h1>Welcome, <?php echo $_SESSION['todo_user'];?></h1>
+                <a href="./logout">Logout</a>
+
+            </div>
+
+            <div class="content-container">
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sequi. Fugiat distinctio fuga vitae veniam, beatae voluptatem animi aliquid, dolorem natus, magnam ipsam dolores suscipit. Dolorum totam aliquam deserunt accusantium.</p>
+                </div>
+                                
+            </div>
+
+            <div class="end">
+
+                <form action="" method="post">
+                    <input type="text" name="task_name" placeholder="Enter your task name...">
+                    <button type="submit" name="add_task">Add Task</button>
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
 </body>
 </html>
